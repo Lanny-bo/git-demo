56ba79e7c50bb3921815fac90567a16da4ddd321
Lanny-bo <62239801+Lanny-bo@users.noreply.github.com>
2023-03-12 21:11:04 +0800

    Update GitTest.java

--- src/main/java/com/git/GitTest.java
+++ src/main/java/com/git/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("你好啊");
         System.out.println("hot-fix");
         System.out.println("push test");
+        git@github.com:Lanny-bo/git-demo.git
     }
 }
