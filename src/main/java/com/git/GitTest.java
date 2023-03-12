2837bf5801037390b73c3f7f9cbb79ff9ae734f5
alan <923861941@qq.com>
2023-03-12 21:02:07 +0800

    push test

--- src/main/java/com/git/GitTest.java
+++ src/main/java/com/git/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("你好啊");
         System.out.println("hot-fix");
+        System.out.println("push test");
     }
 }
